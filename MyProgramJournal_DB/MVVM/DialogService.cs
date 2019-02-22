df89568b58516ad21462747ba4c4a3af29e7fa7c
Andrew Andrew <fasgetz@hotmail.com>
2019-02-22 15:42:48 +0300

    Create on Client MVVM Architecture

---
+++ MyProgramJournal_DB/MVVM/DialogService.cs
@@ -0,0 +1,84 @@
+﻿using MyProgramJournal_DB.MVVM.Interfaces;
+using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyProgramJournal_DB.MVVM
+{
+    class DialogService : IDialogService
+    {
+        // Вызываем новый MessageBox с сообщением string
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+
+
+        // Ассемблирование для определения какую страницу/окно открывать из библиотеки классов
+        public object CreateWindow(string fullClassName)
+        {
+            if (fullClassName != null)
+            {
+                Assembly asm = this.GetType().Assembly;
+
+                object wnd = asm.CreateInstance(fullClassName);
+                if (wnd == null)
+                {
+                    throw new TypeLoadException("Unable to create window: " + fullClassName);
+                }
+                return wnd;
+            }
+
+            return null;
+        }
+
+        // Создание новой страницы
+        public Page newPage(string PageName)
+        {
+            try
+            {
+                string Window = "MyProgramJournal_DB.View." + PageName; // Ссылка на окно, которое будем создавать
+
+                Page MyPage = (Page)CreateWindow(Window); // Ссылка на страницу
+                MyPage.DataContext = this;
+                if (MyPage != null)
+                    return MyPage;
+            }
+            catch(Exception ex)
+            {
+                ShowMessage(ex.ToString());
+            }
+
+            return null;
+        }
+
+        // Создание нового окна
+        public void newWindow(string WindowName)
+        {
+            try
+            {
+                string Window = "MyProgramJournal_DB.View." + WindowName; // Ссылка на окно, которое будем создавать
+
+                Window NewWindow = (Window)CreateWindow(Window); // Получаем ссылку на новое окно
+                Window OldWindow = App.Current.MainWindow; // Ссылку на старое окно
+                NewWindow.DataContext = OldWindow.DataContext; // Дата контекст передаем в новое окно
+
+                OldWindow.Close(); // Закрываем старое окно
+                NewWindow.Show(); // Открываем новое окно
+
+                App.Current.MainWindow = NewWindow; // Новое окно делаем новым и основным окном
+
+
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.ToString());
+            }
+
+
+        }
+
+    }
+}
---
+++ MyProgramJournal_DB/ViewModel/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using MyProgramJournal_DB.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyProgramJournal_DB.ViewModel
+{
+
+    class ViewModel : ObservableObject
+    {
+        private string _text;
+        public string text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                RaisePropertyChangedEvent("text");
+            }
+        }
+
+
+        private void gona()
+        {
+            text = "324234234";
+        }
+
+        // Команда кек
+        public ICommand kek
+        {
+            get
+            {
+                return new DelegateCommand(gona);
+            }
+        }
+    }
+
+}
